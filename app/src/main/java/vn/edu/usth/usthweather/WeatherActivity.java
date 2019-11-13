536e86522603f807d8252a29caac2c8f442bab6b
daoanhhong <anhhong320@gmail.com>
2019-11-13 14:40:19 +0700

    lab2

---
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -0,0 +1,42 @@
+package vn.edu.usth.usthweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i("Weather ","cloudy");
+    }
+}
